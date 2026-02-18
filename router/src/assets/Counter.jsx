9147ed5f58cf57a9e2a6c7f1a4f7ac181c9cde97
Dhinaseelan <dhinaseeland@gmail.com>
2026-02-19 00:03:09 +0530

    fix:minor update

--- router/src/assets/Counter.jsx
+++ router/src/assets/Counter.jsx
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 
 export const counter = () => {
     const [count,setCount]=useState(0)
+    function add(){
+        setCount(count=> count+1);
+        setCount(count=> count+1);
+    }
   return (
     <>
     <h1>{count}</h1>
-    <button>add 2</button>
+    <button onClick={add}>add 2</button>
     </>
   )
 }
